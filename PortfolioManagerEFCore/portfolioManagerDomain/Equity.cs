b2661a3a0a3e9e641ac470efdedc125e523d1a0d
harshatProject <harsha.t@hotmail.com>
2023-03-09 13:39:14 +0530

    Initial Commit

---
+++ PortfolioManagerEFCore/portfolioManagerDomain/Equity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace portfolioManagerDomain
+{
+    public class Equity
+    {
+        public int Id;
+        public string Name;
+        public string Description;
+        public Equity() { }
+        public DateTime PurchaseDate { get; set; }
+        public double AcquiredPrice { get; set; }
+        public double? SoldPrice { get; set;}
+        public bool isSold { get; set; }
+        public double? OverallPnL { get;set; }
+
+    }
+}
---
+++ PortfolioManagerEFCore/portfolioManagerDomain/portfolio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace portfolioManagerDomain
+{
+    public class portfolio
+    {
+        public int id;
+
+        public string Name;
+
+        public string Description;
+        public List<Equity> Equities { get; set; }
+
+    }
+}
