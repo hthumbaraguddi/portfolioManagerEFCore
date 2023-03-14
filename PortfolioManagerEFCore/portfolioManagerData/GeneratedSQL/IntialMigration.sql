39b2ca5c6841b35d4ac74a5200270be7e7cef988
hthumbaraguddi <harsha.thumbaraguddi@gmail.com>
2023-03-14 12:46:57 +0530

    Initial Migration with Generated SQL

---
+++ PortfolioManagerEFCore/portfolioManagerData/GeneratedSQL/IntialMigration.sql
@@ -0,0 +1,46 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Portfolios] (
+    [PortfolioId] int NOT NULL IDENTITY,
+    [Name] nvarchar(max) NOT NULL,
+    [Description] nvarchar(max) NOT NULL,
+    CONSTRAINT [PK_Portfolios] PRIMARY KEY ([PortfolioId])
+);
+GO
+
+CREATE TABLE [Equity] (
+    [EquityId] int NOT NULL IDENTITY,
+    [Name] nvarchar(max) NOT NULL,
+    [Description] nvarchar(max) NOT NULL,
+    [PurchaseDate] datetime2 NOT NULL,
+    [AcquiredPrice] float NOT NULL,
+    [SoldPrice] float NULL,
+    [isSold] bit NOT NULL,
+    [OverallPnL] float NULL,
+    [PortfolioID] int NOT NULL,
+    CONSTRAINT [PK_Equity] PRIMARY KEY ([EquityId]),
+    CONSTRAINT [FK_Equity_Portfolios_PortfolioID] FOREIGN KEY ([PortfolioID]) REFERENCES [Portfolios] ([PortfolioId]) ON DELETE CASCADE
+);
+GO
+
+CREATE INDEX [IX_Equity_PortfolioID] ON [Equity] ([PortfolioID]);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20230313122532_Initial', N'6.0.0');
+GO
+
+COMMIT;
+GO
+
---
+++ PortfolioManagerEFCore/portfolioManagerData/Migrations/20230313122532_Initial.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace portfolioManagerData.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Portfolios",
+                columns: table => new
+                {
+                    PortfolioId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Portfolios", x => x.PortfolioId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Equity",
+                columns: table => new
+                {
+                    EquityId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PurchaseDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AcquiredPrice = table.Column<double>(type: "float", nullable: false),
+                    SoldPrice = table.Column<double>(type: "float", nullable: true),
+                    isSold = table.Column<bool>(type: "bit", nullable: false),
+                    OverallPnL = table.Column<double>(type: "float", nullable: true),
+                    PortfolioID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Equity", x => x.EquityId);
+                    table.ForeignKey(
+                        name: "FK_Equity_Portfolios_PortfolioID",
+                        column: x => x.PortfolioID,
+                        principalTable: "Portfolios",
+                        principalColumn: "PortfolioId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Equity_PortfolioID",
+                table: "Equity",
+                column: "PortfolioID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Equity");
+
+            migrationBuilder.DropTable(
+                name: "Portfolios");
+        }
+    }
+}
